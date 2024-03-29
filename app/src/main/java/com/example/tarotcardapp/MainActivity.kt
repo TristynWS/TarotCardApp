82722e3a746444442834d9eb0477929110ac24b0
Tristyn <Tristynsanchez88@gmail.com>
2024-03-29 18:50:20 -0400

    Updates to layout

--- app/src/main/java/com/example/tarotcardapp/MainActivity.kt
+++ app/src/main/java/com/example/tarotcardapp/MainActivity.kt
@@ -11,36 +11,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarotcardapp.ui.theme.TarotCardAppTheme
-
-class MainActivity : ComponentActivity() {
+import android.content.Intent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TarotCardAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+    }
+
+    // Method for Learn button click
+    fun onLearnClicked(view: View) {
+        val intent = Intent(this, LearnActivity::class.java)
+        startActivity(intent)
+    }
+
+    // Method for Single Card Reading button click
+    fun onSingleCardClicked(view: View) {
+        val intent = Intent(this, SingleCardActivity::class.java)
+        startActivity(intent)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+class SingleCardActivity {
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TarotCardAppTheme {
-        Greeting("Android")
-    }
+class LearnActivity {
+
 }
